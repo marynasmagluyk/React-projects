d9a8b77396e6098e1f533f473b0681f2111d4747
maryna <merry.kosiv@gmail.com>
2020-11-12 20:07:07 +0200

    ui setup

---
+++ 9-color-generator/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+import Values from 'values.js';
+
+function App() {
+    const [color, setColor] = useState('');
+    const [error, setError] = useState(false);
+    const [list, setList] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+    return <>
+        <section className='container'>
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={color} onChange={(e) => setColor(e.target.value)}
+                placeholder='#f15025'/>
+                <button className='btn' type='submit'>submit</button>
+            </form>
+        </section>
+        <section className='colors'>
+            <h3>list goes here</h3>
+        </section>
+    </>
+}
+
+export default App;
