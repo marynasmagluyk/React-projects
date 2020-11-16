f92ab96359c7135cb64c50dcef3c120d99a34834
maryna <merry.kosiv@gmail.com>
2020-11-16 14:11:40 +0200

    sidebar and modal open close feature

--- 12-sidebar-and-modal/src/Context.js
+++ 12-sidebar-and-modal/src/Context.js
@@ -1,11 +1,33 @@
-import React, {useContext} from 'react'
+import React, {useState, useContext} from 'react'
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    };
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
-        <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+        <AppContext.Provider value={{
+            isModalOpen,
+            isSidebarOpen,
+            openSidebar,
+            openModal,
+            closeModal,
+            closeSidebar,
+        }}>{children}</AppContext.Provider>
     );
 };
 
--- 12-sidebar-and-modal/src/Home.js
+++ 12-sidebar-and-modal/src/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { FaBars } from 'react-icons/fa'
+import {FaBars} from 'react-icons/fa'
 import {useGlobalContext} from './Context'
 
 const Home = () => {
-    const data = useGlobalContext();
+    const {openSidebar, openModal} = useGlobalContext();
     return <main>
-        <button className='sidebar-toggle'>
+        <button className='sidebar-toggle'
+                onClick={openSidebar}>
             <FaBars/>
         </button>
-        <button className='btn'>show modal</button>
+        <button className='btn'
+                onClick={openModal}>show modal
+        </button>
     </main>
 }
 
