a002abfbce622eae46d7378630006a36958d1acf
maryna <merry.kosiv@gmail.com>
2020-11-18 18:37:07 +0200

    increase/decrease functionality

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -4,9 +4,28 @@ const reducer = (state, action) => {
     }
     if (action.type === 'REMOVE') {
         return {
-            ...state,cart: state.cart.filter((cartItem) =>
+            ...state, cart: state.cart.filter((cartItem) =>
                 cartItem.id !== action.payload),
         }
+    }
+    if (action.type === 'INCREASE') {
+        const tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+            return cartItem;
+        });
+        return {...state, cart: tempCart};
+    }
+
+    if (action.type === 'DECREASE') {
+        const tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                return {...cartItem, amount: cartItem.amount - 1}
+            }
+            return cartItem;
+        }).filter((cartItem) => cartItem.amount !== 0);
+        return {...state, cart: tempCart};
     }
 
     return state;
