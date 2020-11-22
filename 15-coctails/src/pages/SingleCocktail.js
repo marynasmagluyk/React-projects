9c8c74ae459272c2f681d9e3aedf764cfd7f7f92
maryna <merry.kosiv@gmail.com>
2020-11-22 10:09:11 +0200

    fix file names

--- 15-coctails/src/pages/SingleCocktail.js
+++ 15-coctails/src/pages/SingleCocktail.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Loading from '../components/Loading'
 import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
 const SingleCocktail = () => {
     return (
@@ -9,6 +9,6 @@ const SingleCocktail = () => {
             <h2>single cocktail page </h2>
         </div>
     )
-}
+};
 
 export default SingleCocktail
