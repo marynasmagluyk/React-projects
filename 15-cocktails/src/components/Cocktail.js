1e5a5f470217ca5dbf9c520c0b5799f5e62dff24
maryna <merry.kosiv@gmail.com>
2020-11-22 16:22:57 +0200

    added coctails search form

--- 15-cocktails/src/components/Cocktail.js
+++ 15-cocktails/src/components/Cocktail.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Cocktail = ({id, img, name, info, glass}) => {
     return (
         <article className='cocktail'>
             <div className='img-container'>
-                <img src={img}
-                     alt={name}/>
+                <Link to={`/cocktail/${id}`}>
+                    <img src={img}
+                         alt={name}/>
+                </Link>
             </div>
             <div className='cocktail-footer'>
                 <h3>{name}</h3>
                 <h4>{glass}</h4>
                 <p>{info}</p>
-                <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
+                <Link to={`/cocktail/${id}`}
+                      className='btn btn-primary btn-details'>
                     details
                 </Link>
             </div>
         </article>
-    )
+)
 }
 
 export default Cocktail
--- 15-cocktails/src/components/SearchForm.js
+++ 15-cocktails/src/components/SearchForm.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import { useGlobalContext } from '../context'
+import {useGlobalContext} from '../context'
 
 const SearchForm = () => {
-     const {setSearchTerm} = useGlobalContext();
+    const {setSearchTerm} = useGlobalContext();
+    const searchValue = React.useRef();
 
+    React.useEffect(() => {
+        searchValue.current.focus()
+    },[]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const searchCocktail = () => {
+        setSearchTerm(searchValue.current.value)
+    };
     return (
-        <div>
-            <h2>search form component</h2>
-        </div>
+        <section className='section search'>
+            <form className='search-form' onSubmit={handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor="name">serch your favourite cocktail</label>
+                    <input type="text"
+                           id='name'
+                           ref={searchValue}
+                           onChange={searchCocktail}/>
+                </div>
+            </form>
+        </section>
     )
 };
 
